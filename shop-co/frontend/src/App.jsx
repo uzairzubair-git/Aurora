aba857448c15ce4bcc7dcba32e11c29582a55f18
uzairzubair-git <uzairzubaircomp@gmail.com>
2026-08-22 09:27:16 +0500

    3

--- shop-co/frontend/src/App.jsx
+++ shop-co/frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import AnnouncementBar from "./Components/AnnouncementBar/AnnouncementBar";
 import Navbar from "./Components/Navbar/Navbar";
+// import Hero from "./Components/Hero/Hero.jsx";
+
 function App() {
   return (
     <div>
       <AnnouncementBar />
       <Navbar />
+      <Hero />
     </div>
   );
 }
